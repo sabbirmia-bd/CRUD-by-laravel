5dd5946cfd953fa552f45f0418ea09dfc9f7d8cf
sabbir24 <prsabbir9@gmail.com>
2020-04-08 23:59:12 +0600

    color change

--- resources/views/crudusers/user_view.blade.php
+++ resources/views/crudusers/user_view.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col-md-12">
         <div class="card">
-          <div class="card-header">
+          <div class="card-header bg-info">
             View Page
           </div>
           <div class="card-body">
